34f08606f9aab6d77e683f494fb0f663bbf4cde1
HyeonbinSa <shb1833@gmail.com>
2021-12-16 20:27:10 +0900

    docs: README.md 수정(구간 관리 예외 조건 상세 작성)

--- src/main/java/subway/validator/IntervalValidator.java
+++ src/main/java/subway/validator/IntervalValidator.java
@@ -11,23 +11,6 @@ import java.util.List;
 public class IntervalValidator {
     private static final List<String> menuList = Arrays.asList("1", "2", "B");
 
-    /**
-     * 1. 입력
-     * 1. 노선 입력 시 검증
-     * - 선택한 노선이 없을 경우
-     * 2. 역 이름 입력 시 검증
-     * - 해당 역이  Stations에 존재하지 않는 경우
-     * - 노선에 이미 등록되어 있는 경우
-     * - 입력할 수 없는 위치에 입력했을 경우
-     * 2. 삭제
-     * 1. 노선 입력 시 검증
-     * - 선택한 노선이 없는 경우
-     * 2. 해당 노선의 사이즈가 2보다 작을 경우
-     * 3. 역 이름 입력  검증
-     * - 해당 역이 존재하지 않는 경우
-     * - 노선 내에 없는 경우
-     * - 해당 역 이름 제거 시 1개 이하가 될 경우
-     */
     public void validateSelectMenu(String menu) {
         if (!menuList.contains(menu)) {
             throw new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다.");
